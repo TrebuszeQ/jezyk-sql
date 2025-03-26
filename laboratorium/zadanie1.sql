35ba55f37e59661e50fed4aa4916e123602e7acb
admin <trebuszeq@gmail.com>
2025-03-26 19:32:08 +0100

    zad1

---
+++ laboratorium/zadanie1.sql
@@ -0,0 +1,70 @@
+-- 
+USE northwind
+GO
+
+SELECT *
+FROM Customers;
+
+-- 
+USE northwind
+GO
+
+SELECT *
+FROM Orders
+WHERE ShipCountry LIKE 'germany';
+
+-- 
+USE northwind
+GO
+
+SELECT TOP 5 *
+FROM Products
+ORDER BY UnitPrice DESC;
+
+-- 
+USE northwind
+GO
+
+SELECT DISTINCT COUNT(CustomerID) as 'ClientsSum',
+    ShipCountry
+FROM Orders
+GROUP BY ShipCountry;
+
+-- 
+USE northwind
+GO
+
+SELECT *
+FROM Orders
+WHERE YEAR(OrderDate) = '1997';
+
+-- 
+USE northwind
+GO
+
+SELECT DISTINCT CustomerID
+FROM Customers
+WHERE CustomerID NOT IN (
+    SELECT DISTINCT CustomerID
+    FROM Orders
+)
+
+-- 
+USE northwind
+GO
+
+SELECT AVG(UnitPrice) AS 'AverageUnitPrice'
+FROM Products;
+
+-- 
+USE northwind
+GO
+
+SELECT TOP 1 Customer
+FROM Orders (
+    SELECT COUNT(OrderID) 'UniqueOrdersCount',
+        CustomerID
+    FROM Orders
+    GROUP BY CustomerID
+    ORDER BY UniqueOrdersCount DESC
+)
